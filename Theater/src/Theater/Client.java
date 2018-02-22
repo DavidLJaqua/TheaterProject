e9b170bd42517e68951caf6355e51c3eb70557d6
DavidLJaqua <david.jaqua@hotmail.com>
2018-02-22 16:36:28 -0600

    Comments and minor changes to Client classes

--- Theater/src/Theater/Client.java
+++ Theater/src/Theater/Client.java
@@ -9,6 +9,7 @@ import java.io.Serializable;
  */
 public class Client implements Serializable {
 
+	private static final long serialVersionUID = 1L;
 	private String name, address, phoneNum, clientID;
 	private static final String CLIENT_STRING = "Cli";
 	private int balance;
--- Theater/src/Theater/ClientIDServer.java
+++ Theater/src/Theater/ClientIDServer.java
@@ -5,7 +5,7 @@ import java.io.ObjectInputStream;
 import java.io.Serializable;
 
 /**
- * ClientIDServer class servers as a way of getting a new, unique ID for client creation.
+ * ClientIDServer class serves as a way of getting a new, unique ID for client creation.
  * Will be saved along with the Theater class when the program is shutdown.
  * @author David Jaqua
  */
@@ -18,10 +18,17 @@ public class ClientIDServer implements Serializable{
     private static ClientIDServer serverInstance;
     private int idCounter;
 
+    /**
+     * Creates singleton instance of ClientIDServer
+     */
     private ClientIDServer() {
             idCounter = 1;
     }
 
+    /**
+     * Retrieves the singleton instance of ClientIDServer
+     * @return ClientIDServer instance
+     */
     public static ClientIDServer instance(){
             if (serverInstance == null){
                     return (serverInstance = new ClientIDServer());
@@ -29,9 +36,9 @@ public class ClientIDServer implements Serializable{
                     return serverInstance;
             }
     }
+    
     /**
     * Retrieves the server object
-    * 
     * @param input input stream for deserialization.
     */
     public static void retrieve(ObjectInputStream input) {
